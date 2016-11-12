162ccbfafded94350825fc734878cc84169e4711
nuji <jiruttigarn@gmail.com>
2016-11-12 12:04:53 +0700

    intent Signup

--- app/src/main/java/kku/jeji/nuji/nujikku/MainActivity.java
+++ app/src/main/java/kku/jeji/nuji/nujikku/MainActivity.java
@@ -1,13 +1,35 @@
 package kku.jeji.nuji.nujikku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget หรือ การผูก widget กับตัวแปล
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton= (Button) findViewById(R.id.button);
+
+        // Sign Up Controlier
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+
+            }
+        });
+
+
+    } // Main Method หรือ Method หลัก
+} //Main Class
